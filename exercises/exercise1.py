2e714c828117a964feb33f6fe9e21014423b039e
helenakohl <helenachristina@gmx.de>
2023-05-22 17:03:54 +0200

    exercise1.py

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 airports = pd.read_csv("https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv", sep=";")
 
 airports.to_sql('airports', 'sqlite:///airports.sqlite', if_exists='replace', index=False)
