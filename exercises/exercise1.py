677dd9f4183f8697410b10da38e335f5b5733bb8
helenakohl <helenachristina@gmx.de>
2023-05-24 13:46:42 +0200

    exercise 1

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 import ssl
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
 airports = pd.read_csv("https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv", sep=";")
