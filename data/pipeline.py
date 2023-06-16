bc8b271fe33cf3853f636f47c673562eb98cdcef
helenakohl <helenachristina@gmx.de>
2023-06-16 12:21:10 +0200

    updated pipeline

--- data/pipeline.py
+++ data/pipeline.py
@@ -4,17 +4,13 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 # Datasource1: Accident data
-
 accidents = pd.read_csv('https://download.statistik-berlin-brandenburg.de/c2b6d25afa19b607/8d9164595b8b/AfSBBB_BE_LOR_Strasse_Strassenverkehrsunfaelle_2021_Datensatz.csv', sep=';')
 
+# drop irrlevant columns
+irrelevant = ['LINREFX', 'LINREFY']
+accidents = accidents.drop(columns=irrelevant)
+
 # Convert datataypes
-
-accidents["LINREFX"] = [float(str(i).replace(",", ".")) for i in accidents["LINREFX"]]
-accidents["LINREFX"] = pd.to_numeric(accidents["LINREFX"])
-
-accidents["LINREFY"] = [float(str(i).replace(",", ".")) for i in accidents["LINREFY"]]
-accidents["LINREFY"] = pd.to_numeric(accidents["LINREFY"])
-
 accidents["XGCSWGS84"] = [float(str(i).replace(",", ".")) for i in accidents["XGCSWGS84"]]
 accidents["XGCSWGS84"] = pd.to_numeric(accidents["XGCSWGS84"])
 
@@ -25,37 +21,53 @@ accidents.to_sql('accidents', 'sqlite:///accidents.sqlite', if_exists='replace',
 
 
 # Datasource2: Traffic data
+# This dataset is very big and therefore needs to be reduced for the anaysis, this is done in two ways: 
+
+# 1. data is available in seperate files for each month and we can reduce the time frame to one month
+
+# define month to download
+month = '06'
+
+# traffic data for June (month with highest number of accidents)
+traffic_month = pd.read_csv(f'https://mdhopendata.blob.core.windows.net/verkehrsdetektion/2021/Detektoren%20(einzelne%20Fahrspur)/det_val_hr_2021_{month}.csv.gz', compression='gzip', sep=';')
+
+# convert data types
+traffic_month['tag'] = pd.to_datetime(traffic_month['tag'], format="%d.%m.%Y")
 
 # load information for every traffic sensor
 traffic_sensors = pd.read_excel('https://mdhopendata.blob.core.windows.net/verkehrsdetektion/Stammdaten_Verkehrsdetektion_2022_07_20.xlsx')
 
-# traffic data for June (month with highest number of accidents)
-traffic_june = pd.read_csv('https://mdhopendata.blob.core.windows.net/verkehrsdetektion/2021/Detektoren%20(einzelne%20Fahrspur)/det_val_hr_2021_06.csv.gz', compression='gzip', sep=';')
-
 # add information for traffic sensor
-traffic_june = pd.merge(left=traffic_june, right=traffic_sensors, left_on='detid_15', right_on='DET_ID15')
+traffic_month = pd.merge(left=traffic_month, right=traffic_sensors, left_on='detid_15', right_on='DET_ID15')
 
-# convert data types
-traffic_june['tag'] = pd.to_datetime(traffic_june['tag'], format="%d.%m.%Y")
-
-traffic_june.to_sql('traffic_june', 'sqlite:///traffic_june.sqlite', if_exists='replace', index=False)
-
+# drop irrlevant columns
+irrelevant2 = ['ABBAUDATUM', 'DEINSTALLIERT', 'KOMMENTAR']
+traffic_month = traffic_month.drop(columns=irrelevant2)
 
 
-# Import data for every month of 2021
+traffic_month.to_sql('traffic_month', 'sqlite:///traffic_month.sqlite', if_exists='replace', index=False)
 
-#months = {}
 
-#for i in range (1,10):
-#    months[i] =  pd.read_csv(f'https://mdhopendata.blob.core.windows.net/verkehrsdetektion/2021/Detektoren%20(einzelne%20Fahrspur)/det_val_hr_2021_0{i}.csv.gz', compression='gzip', sep=';')
-    
-#for i in range (10,13):
-#   months[i] =  pd.read_csv(f'https://mdhopendata.blob.core.windows.net/verkehrsdetektion/2021/Detektoren%20(einzelne%20Fahrspur)/det_val_hr_2021_{i}.csv.gz', compression='gzip', sep=';')
+
+# 2. Inlcude all months of 21 and combine them, but aggregate the hourly data to daily averages and drop some columns
+
+months = {}
+
+for i in range (1,10):
+    months[i] =  pd.read_csv(f'https://mdhopendata.blob.core.windows.net/verkehrsdetektion/2021/Detektoren%20(einzelne%20Fahrspur)/det_val_hr_2021_0{i}.csv.gz', compression='gzip', sep=';')
+    months[i] = months[i].groupby(["detid_15","tag"])[["qualitaet","q_kfz_det_hr","v_kfz_det_hr","q_pkw_det_hr","v_pkw_det_hr","q_lkw_det_hr","v_lkw_det_hr"]].mean()
+
+for i in range (10,13):
+    months[i] =  pd.read_csv(f'https://mdhopendata.blob.core.windows.net/verkehrsdetektion/2021/Detektoren%20(einzelne%20Fahrspur)/det_val_hr_2021_{i}.csv.gz', compression='gzip', sep=';')
+    months[i] = months[i].groupby(["detid_15","tag"])[["qualitaet","q_kfz_det_hr","v_kfz_det_hr","q_pkw_det_hr","v_pkw_det_hr","q_lkw_det_hr","v_lkw_det_hr"]].mean()
 
 # Combine all data for 2021
-#traffic = pd.concat(months)
+traffic_2021 = pd.concat(months)
 
 # add information for traffic sensor
-#traffic = pd.merge(left=traffic, right=traffic_sensors, left_on='detid_15', right_on='DET_ID15')
+traffic_2021 = pd.merge(left=traffic_2021, right=traffic_sensors, left_on='detid_15', right_on='DET_ID15')
 
-#traffic.to_sql('traffic', 'sqlite:///traffic.sqlite', if_exists='replace', index=False)
+# drop irrlevant columns
+traffic_2021 = traffic_2021.drop(columns=irrelevant2)
+
+traffic_2021.to_sql('traffic_2021', 'sqlite:///traffic_2021.sqlite', if_exists='replace', index=False)
--- project/test.py
+++ project/test.py
@@ -11,8 +11,14 @@ if file_exists(file_path1):
 else:
     print("Test not succesfull.The file does not exist.")
 
-file_path2 = "./traffic_june.sqlite"
+file_path2 = "./traffic_month.sqlite"
 if file_exists(file_path2):
     print(f"Test successfull. File found under the following path: {file_path2}")
+else:
+    print("Test not succesfull.The file does not exist.")
+
+file_path3 = "./traffic_2021.sqlite"
+if file_exists(file_path3):
+    print(f"Test successfull. File found under the following path: {file_path3}")
 else:
     print("Test not succesfull.The file does not exist.")
