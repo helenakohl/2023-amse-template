486e179c17eecdb15b80abc7036dc443fee89fea
helenakohl <helenachristina@gmx.de>
2023-06-25 16:41:38 +0200

    updated piepline

--- data/pipeline.py
+++ data/pipeline.py
@@ -18,6 +18,19 @@ accidents["XGCSWGS84"] = pd.to_numeric(accidents["XGCSWGS84"])
 accidents["YGCSWGS84"] = [float(str(i).replace(",", ".")) for i in accidents["YGCSWGS84"]]
 accidents["YGCSWGS84"] = pd.to_numeric(accidents["YGCSWGS84"])
 
+# rename values in categorical data
+day_mapping = {1: 'Sonntag', 2: 'Montag', 3: 'Dienstag', 4: 'Mittwoch', 5: 'Donnerstag', 6: 'Freitag', 7: 'Samstag'}
+cat_mapping = {1: 'Unfall mit Getöteten', 2: 'Unfall mit Schwerverletzten', 3: 'Unfall mit Leichtverletzten'}
+type_mapping = {1: 'Fahrunfall', 2: 'Abbiegeunfall', 3: 'Einbiegen / Kreuzen-Unfall', 4: 'Überschreiten-Unfall', 5: 'Unfall durch ruhenden Verkehr', 6: 'Unfall im Längsverkehr', 7: 'sonstiger Unfall'}
+light_mapping = {0: 'Tageslicht', 1: 'Dämmerung', 2: 'Dunkelheit'}
+street_mapping = {0: 'trocken', 1: 'nass/feucht', 2: 'winterglatt'}
+
+accidents['UWOCHENTAG'] = accidents['UWOCHENTAG'].replace(day_mapping)
+accidents['UKATEGORIE'] = accidents['UKATEGORIE'].replace(cat_mapping)
+accidents['UTYP1'] = accidents['UTYP1'].replace(type_mapping)
+accidents['UKATEGORIE'] = accidents['UKATEGORIE'].replace(light_mapping)
+accidents['USTRZUSTAND'] = accidents['USTRZUSTAND'].replace(street_mapping)
+
 # create additional column that indicates all involved in the accident
 to_combine = ['IstRad','IstPKW','IstFuss','IstKrad','IstGkfz','IstSonstige']
 
@@ -60,6 +73,13 @@ traffic_month = pd.merge(left=traffic_month, right=traffic_sensors, left_on='det
 irrelevant2 = ['ABBAUDATUM', 'DEINSTALLIERT', 'KOMMENTAR']
 traffic_month = traffic_month.drop(columns=irrelevant2)
 
+# Remove negative values for speed and quantity
+non_negative = ['q_kfz_det_hr','v_kfz_det_hr', 'q_pkw_det_hr', 'v_pkw_det_hr', 'q_lkw_det_hr', 'v_lkw_det_hr']
+
+valid = traffic_month[non_negative].apply(lambda x: all(x >= 0), axis=1)
+
+traffic_month = traffic_month[valid]
+
 # save data to sql database
 traffic_month.to_sql('traffic_month', 'sqlite:///traffic.sqlite', if_exists='replace', index=False)
 
@@ -83,9 +103,13 @@ traffic_2021 = pd.concat(months)
 # add information for traffic sensor
 traffic_2021 = pd.merge(left=traffic_2021, right=traffic_sensors, left_on='detid_15', right_on='DET_ID15')
 
-
 # drop irrlevant columns
 traffic_2021 = traffic_2021.drop(columns=irrelevant2)
+
+# Remove negative values for speed and quantity
+valid = traffic_2021[non_negative].apply(lambda x: all(x >= 0), axis=1)
+
+traffic_2021 = traffic_2021[valid]
 
 # save data to sql database
 traffic_2021.to_sql('traffic_2021', 'sqlite:///traffic.sqlite', if_exists='replace', index=False)
